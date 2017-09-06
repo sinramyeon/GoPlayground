8248c5455cb2ddd74efd2a874057b96652163b76
hero0926 <esistkalt@naver.com>
2017-09-06 17:42:50 +0900

    지금비와? 기능 추가

--- WeatherAPI/weather.go
+++ WeatherAPI/weather.go
@@ -1,3 +1,15 @@
+package WeatherAPI
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
 
 //Disclaimer
 
@@ -12,6 +24,9 @@
 
 //간편날씨 주소
 const Simpleurl = "http://apis.skplanetx.com/weather/summary?version=1&lat=%s&lon=%s&stnid=%s"
+
+// 분별날씨 주소
+const Rightnowurl = "http://apis.skplanetx.com/weather/current/minutely?lat=%s&lon=%s&village=&county=&stnid=&city=&version=1"
 
 // 시간별 현재날씨 주소
 const Nowurl = "http://apis.skplanetx.com/weather/current/hourly?lon=127.02583&village=&county=korea&lat=%s&lon=%s&city=%s&version=1"
@@ -87,7 +102,35 @@ type Grid struct {
 	Village string `json:"village"`
 }
 
-// 자외선 지수
+// 3. 분별 날씨
+type MinWeather struct {
+	Weather Minutely `json:"weather"`
+}
+
+type Minutely struct {
+	Minutely []struct {
+		Station     Station     `json:"station"`
+		Temperature Temperature `json:"temperature"`
+		Sky         Sky         `json:"sky"`
+		Rain        Rain        `json:"rain"`
+	} `json:"minutely"`
+}
+
+type Rain struct {
+	SinceOntime   string `json:"sinceOntime"`
+	SinceMidnight string `json:"sinceMidnight"`
+	Last10min     string `json:"last10min"`
+	Last15min     string `json:"last15min"`
+	Last30min     string `json:"last30min"`
+}
+
+type RightNow struct {
+	Temperature Temperature
+	Sky         Sky
+	Rain        Rain
+}
+
+// 4.자외선 지수
 type UVWeather struct {
 	Weather Windex `json:"weather"`
 }
@@ -111,8 +154,7 @@ type Day struct {
 	Comment  string `json:"comment"`
 }
 
-// 미세먼지
-
+// 5.미세먼지
 type Pollution struct {
 	Weather Dust `json:"weather"`
 }
@@ -308,7 +350,7 @@ func GetTodayPollution() map[string]string {
 	for _, v := range pollution.Weather.Dust {
 
 		var today = v.Time
-
+		//띄어쓰기를 기준으로 그 후에 있는 날짜만 가져왔어요.
 		pos := strings.Index(today, " ")
 		today = today[0:pos]
 
@@ -317,4 +359,34 @@ func GetTodayPollution() map[string]string {
 	}
 
 	return Pollution
+}
+
+// 지금비와!?
+// 분별 날씨를 얻어와서 10, 20, 30분 전과 지금의 강수량을 갖고옵니다.
+func IsItRaining() map[string]RightNow {
+
+	jsonStr := CallAPI()
+
+	seocho := fmt.Sprintf(Rightnowurl, "37.508214", "127.056541")
+	body := ReqWeather(seocho, jsonStr)
+
+	// 받아온 json값을 처리합니다.
+	var minweather MinWeather
+	if err := json.Unmarshal(body, &minweather); err != nil {
+		log.Fatal(err)
+	}
+
+	rightnow := make(map[string]RightNow)
+
+	for _, v := range minweather.Weather.Minutely {
+
+		Name := v.Station.Name
+
+		rightnow[Name] = RightNow{
+			Rain:        v.Rain,
+			Sky:         v.Sky,
+			Temperature: v.Temperature,
+		}
+	}
+	return rightnow
 }
